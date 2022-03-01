8ee867af02081eded4a5daf8e65c7c4fc57a3a51
kyondoku <kyondoku@gmail.com>
2022-03-01 23:58:08 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return (
-    <h1>Hello :)</h1>
-  );
+  const [videos, setVideos] = useState([]);
+  
+  useEffect(()=>{
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDCN9su0PURgu0niZxmIgpCl_5TW9weOfI", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []); // [] -> 마운트가 되었을때 한번만 호출 
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
