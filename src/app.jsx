6f4e92f8948bea8e90244c7fea0f0caa2161b575
kyondoku <kyondoku@gmail.com>
2022-03-07 01:47:48 +0900

    SearchHeader 컴포넌트 적용

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import './app.css';
+import styles from './app.module.css';
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
 function App() {
@@ -20,7 +21,12 @@ function App() {
       .catch(error => console.log('error', error));
   }, []); // [] -> 마운트가 되었을때 한번만 호출 
 
-  return <VideoList videos={videos} />;
+  return (
+    <div className={styles.app}>
+      <SearchHeader />
+      <VideoList videos={videos} />;
+    </div>
+  );
 }
 
 export default App;
--- src/components/nav_bar/nav_bar.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NavBar = (props) => (
-            
-    );
-
-export default NavBar;
