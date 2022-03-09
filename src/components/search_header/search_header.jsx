8360dce0cd7bcb25a0c3f23ee12a77414a32432f
kyondoku <kyondoku@gmail.com>
2022-03-09 21:50:16 +0900

    header 기본 css 적용 완료

--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -1,6 +1,18 @@
 import styles from './search_header.module.css';
 import React from 'react';
 
-const SearchHeader = props => <h1>Header</h1>;
-
+const SearchHeader = props => {
+    return (
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <img className={styles.img} src="/images/logo.png" alt="logo" />
+                <h1 className={styles.title}>Youtube</h1>
+            </div>
+            <input className={styles.input} type="search" placeholder="Search..." />
+            <button className={styles.button} type="submit">
+                <img className={styles.buttonImg} src="/images/search.png" alt="" />
+            </button>
+        </header>
+    )
+}
 export default SearchHeader;
